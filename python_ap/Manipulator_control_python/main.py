a75c9ac86c6d22f25beb9f825b78fc618b90a108
OnisOris <55390311+OnisOris@users.noreply.github.com>
2023-02-25 15:05:27 +0300

    Добавление простейшего консольного приложения с командами для движения и калибровки манипулятора

--- python_ap/Manipulator_control_python/main.py
+++ python_ap/Manipulator_control_python/main.py
@@ -13,7 +13,39 @@ arduino_port = 7
 ################# Конец настроек #################
 
 robot = Manipulator(f'COM{teensy_port}', f'COM{arduino_port}', baud)
-position = [68.944, 0.0, 733.607, -90.0, 1.05, -90.0]
+while (True):
+    inp = input("Введите команду \n")
+    inp_c = inp.split()
+    try:
+        if(inp ==  "exit"):
+            break
+        elif(inp ==  "calib"):
+            robot.auto_calibrate()
+        elif (inp_c[0] == "jog"):
+            if (int(inp_c[1]) >= 0 and int(inp_c[1]) <= 5):
+                robot.jog_joint(robot.joints[int(inp_c[1])-1], 20,  int(inp_c[2]))
+            else:
+                print("Звеньев все 6, введите число от 1 до 6")
+        elif(inp ==  "help"):
+            print("move_x [расстояние в мм] - передвижение по оси x в [мм]\n ")
+            print("move_y [расстояние в мм] - передвижение по оси y в [мм]\n ")
+            print("move_z [расстояние в мм] - передвижение по оси z в [мм]\n ")
+            print("calib - автокалибровка\n ")
+            print("jog [номер джойнта 1-6]\n ")
+        elif (inp_c[0] == "move_x"):
+                robot.move_x(int(inp_c[1]))
+        elif (inp_c[0] == "move_y"):
+                robot.move_y(int(inp_c[1]))
+        elif (inp_c[0] == "move_z"):
+                robot.move_z(int(inp_c[1]))
+        else:
+            print("Неправильная команда")
+    except ValueError:
+        print("Из этого положения нельзя передвинуться в новое")
+
+
+
+#position = [68.944, 0.0, 733.607, -90.0, 1.05, -90.0]
 #logger.debug(robot.calculate_direct_kinematics_problem2(8.391331785652922e-05, -89.99514827193128, 1.0385111119522037, 0.013274359904453395, 0.006637160177256035, -0.01319046058787876))
 # robot.joints[0].current_joint_angle = 170
 # robot.joints[1].current_joint_angle = -129.6
@@ -23,8 +55,8 @@ position = [68.944, 0.0, 733.607, -90.0, 1.05, -90.0]
 # robot.joints[5].current_joint_angle = 148.1
 # robot.print()
 #print(DEFAULT_SETTINGS['DH_t_1'])
-robot.auto_calibrate()
-#robot.print()
+#robot.auto_calibrate()
+##robot.print()
 #robot.print()
 # logger.debug(robot.joints[0].current_joint_angle)
 # robot.jog_joint(robot.joints[0], 30, 40)
@@ -36,7 +68,7 @@ robot.auto_calibrate()
 #position = [161.29, 15.261, 713.24, -81.03, 44.104, -88.58]
 
 # robot.move_xyz(position)
-robot.print()
+#robot.print()
 # time.sleep(2)
 # position = [299.45, 5.4327, 329.97, -98.96, 74, -82.43]
 #
@@ -46,16 +78,16 @@ robot.print()
 # robot.jog_joint(robot.joints[1], "30", 30)
 # robot.jog_joint(robot.joints[2], "30", 30)
 # robot.print()
-print("----------------------------")
+#print("----------------------------")
 # robot.move_z(20)
 # robot.print()
 # print("----------------------------")
 #  need_angles=(90.0, -54.4326657322994, 14.139687841074192, -1.0585094009335781e-14, 11.29297789122522, 0.0)
 # 2023-01-31 16:01:58.363 | DEBUG    | manipulator:move_xyz:456 - MJA10B1309C0943D00E1247F10S30G15H10I20K5
 # 2023-01-31 16:01:58.364 | DEBUG    | manipulator:print:1137 - x = 0.15468345111515427 y = 443.13542747242195 z = 579.3600934009104 theta = -179.98 phi = 61.019999999999996 psi = -90.00000000000001
-robot.move_xyz([-1.337, 399.893, 329.457, -179.98, 74.28, -100.059])
-robot.move_z(-30)
-robot.print()
+# robot.move_xyz([-1.337, 399.893, 329.457, -179.98, 74.28, -100.059])
+# robot.move_z(-30)
+# robot.print()
 
 #robot.jog_joint(robot.joints[0], "30", 45)
 #robot.move_xyz(position)
