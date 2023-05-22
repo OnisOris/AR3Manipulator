071c12afee158da3f7bc089a628d44886fca0b07
OnisOris <onisoris@yandex.ru>
2023-05-22 16:45:14 +0300

    Сеттеры в класс joint

--- python_ap/Manipulator_control_python/joint.py
+++ python_ap/Manipulator_control_python/joint.py
@@ -6,7 +6,7 @@ class Joint:
     def __init__(self, number_joint, positive_angle_limit, negative_angle_limit, step_limit):
         self.name_joint = None
         self.motor_direction = 0
-        self.motor_dir = 1 # позже нужно удалить
+        self.motor_dir = 1  # параметр, задающий сторону полуокружности вращения
         self.number_joint = number_joint
         self.positive_angle_limit = float(positive_angle_limit)
         self.negative_angle_limit = float(negative_angle_limit)
@@ -16,11 +16,17 @@ class Joint:
         self.current_joint_angle = 0
         self.degrees_per_step = (abs(self.positive_angle_limit) + abs(self.negative_angle_limit)) / self.step_limit  # град/шаг
 
+    def set_name_joint(self, name_joint):
+        self.name_joint = name_joint
+
     def get_current_joint_step(self):
         return self.current_joint_step
 
     def get_current_joint_angle(self):
         return self.current_joint_angle
+
+    def get_name_joint(self) -> str:
+        return self.name_joint
 
     def change_current_joint_step(self, current_joint_step) -> None:
         self.current_joint_step = current_joint_step
@@ -28,13 +34,11 @@ class Joint:
     def change_current_joint_angle(self, current_joint_angle) -> None:
         self.current_joint_angle = current_joint_angle
 
-    def set_name_joint(self, name_joint):
-        self.name_joint = name_joint
+    def change_positive_angle_limit(self, positive_angle_limit) -> None:
+        self.positive_angle_limit = positive_angle_limit
 
-    def get_name_joint(self) -> str:
-        return self.name_joint
+    def change_negative_angle_limit(self, negative_angle_limit) -> None:
+        self.negative_angle_limit = negative_angle_limit
 
     def __str__(self):
         return f'Joint_{self.number_joint}({self.get_name_joint()})'
-
-
