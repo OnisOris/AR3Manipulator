9854dd3329674d83bb7aa2d0ef7883484ca99424
OnisOris <55390311+OnisOris@users.noreply.github.com>
2023-06-05 18:39:37 +0300

    Папка Scripts_UV

--- python_ap/Manipulator_control_python/main.py
+++ python_ap/Manipulator_control_python/main.py
@@ -1,4 +1,4 @@
 from manipulator import Manipulator
 robot = Manipulator(test_mode=False)
-robot.read_points('/Scripts/calibration.txt')
-robot.read_points('/Scripts/opening_cap.txt')
+robot.read_points('/Scripts_UV/calibration.txt')
+robot.read_points('/Scripts_UV/opening_cap.txt')
