d47c278bed12c33e5b94693a6c10e0567faf39a8
OnisOris <55390311+OnisOris@users.noreply.github.com>
2023-06-05 19:13:40 +0300

    Обновление скриптов, main и README

--- python_ap/Manipulator_control_python/main.py
+++ python_ap/Manipulator_control_python/main.py
@@ -1,4 +1,33 @@
+import time
 from manipulator import Manipulator
+
+
 robot = Manipulator(test_mode=False)
-robot.read_points('./Scripts_UV/calibration.txt')
+
+
+robot.auto_calibrate()
+
+robot.null_position()
+
+time.sleep(5)
+
 robot.read_points('./Scripts_UV/opening_cap.txt')
+
+robot.read_points('./Scripts_UV/calibration.txt')
+
+robot.read_points('./Scripts_UV/take_cuvette_1.txt')
+
+robot.read_points('./Scripts_UV/move_to_UV.txt')
+
+robot.read_points('./Scripts_UV/take_cuvette_2.txt')
+
+robot.read_points('./Scripts_UV/closing_cap.txt')
+
+robot.read_points('./Scripts_UV/calibration.txt')
+
+robot.read_points('./Scripts_UV/opening_cap.txt')
+
+robot.read_points('./Scripts_UV/calibration.txt')
+
+robot.read_points('./Scripts_UV/moving_to_base.txt')
+
